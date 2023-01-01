98989d0845cadcb960e01291ed428edcc32e6f05
James Shiow <jamesshiow@gmail.com>
2023-01-01 22:14:40 +0800

    update google analytics tag

--- components/common/Head/Head.tsx
+++ components/common/Head/Head.tsx
@@ -1,17 +1,24 @@
-import { SEO } from '@components/common'
+import { SEO } from "@components/common";
+import Script from "next/script";
 
 const Head = () => {
-  return (
-    <SEO>
-      <meta
-        key="viewport"
-        name="viewport"
-        content="width=device-width, initial-scale=1"
-      />
-      <link rel="manifest" href="/site.webmanifest" key="site-manifest" />\
-      <link rel="icon" href="/favicon.png" />
-    </SEO>
-  )
-}
+	return (
+		<SEO>
+			<meta key="viewport" name="viewport" content="width=device-width, initial-scale=1" />
+			<link rel="manifest" href="/site.webmanifest" key="site-manifest" />\
+			<link rel="icon" href="/favicon.png" />
+			{/* Global Site Tag (gtag.js) - Google Analytics */}
+			<Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+			<Script id="google-analytics" strategy="lazyOnload">
+				{`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });`}
+			</Script>
+		</SEO>
+	);
+};
 
-export default Head
+export default Head;
